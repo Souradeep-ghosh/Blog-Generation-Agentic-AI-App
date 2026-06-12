c0c310150ffa775276813add866bceb7fb4c269a
Soura <souradeep.ghosh.13@gmail.com>
2026-06-12 18:44:46 +0530

    Add node functionality

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -1,6 +1,7 @@
 from langgraph.graph import StateGraph, START, END
 from src.llms.groqllm import GroqLLM
 from src.states.blogstate import BlogState
+from src.nodes.blog_node import BlogNode
 
 class GraphBuilder:
     def __init__(self,llm):
@@ -12,9 +13,10 @@ class GraphBuilder:
         To build a graph based on various topics. It is to generate blogs.
         
         """
+        self.blog_node_obj = BlogNode(self.llm)
         ##Nodes
-        self.graph.add_node("title_creation", )
-        self.graph.add_node("content_generation",)
+        self.graph.add_node("title_creation", self.blog_node_obj.title_creation)
+        self.graph.add_node("content_generation", self.blog_node_obj.content_generation)
         
         ##Edges
         self.graph.add_edge(START, "title_creation")
---
+++ src/nodes/blog_node.py
@@ -0,0 +1,47 @@
+from src.states.blogstate import BlogState
+
+class BlogNode:
+    """
+    It is a class to represent the blog node
+    """
+    def __init__(self,llm):
+        self.llm = llm
+        
+    def title_creation(self,state:BlogState):
+        """
+        This will be used to create the title for the blog.
+        """
+        if "topic" in state and state["topic"]:
+            
+            prompt= """ 
+                You are an expert blog content writer. Use Markdown formatting.
+                Generate a blog title for the {topic}. This title should be creative and SEO friendly 
+                as well.
+            
+            """
+            
+            system_message= promp.format(topic=state["topic"])
+            response= self.llm.invoke(system_message)
+            
+            return {"blog": {"title": response.content}}
+        
+        
+    def content_generation(self, state:BlogState):
+        """
+        This will be used to create the blog.
+        """
+        if "topic" in state and state["topic"]:
+            system_prompt= """
+            You are an expert blog writer. Use Markdown formatting and 
+            generate a detailed blog content with detailed breakdown for the {topic}
+            """
+            
+            system_message= system_prompt.format(topic=state["topic"])
+            response= self.llm.invoke(system_message)
+            
+            return {"blog": {"title": state["blog"]["title"], "content": response.content}}
+                   
+            
+            
+            
+        
